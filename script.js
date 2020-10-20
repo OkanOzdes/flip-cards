e11fb590563f14b60f7b9eeddeb9fcf754240e9d
a17okaoz <a17okaoz@student.his.se>
2020-10-20 17:54:35 +0200

    finished

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const card = document.querySelector(".card__inner");
+/* const card = document.querySelector(".card__inner");
 
 card.addEventListener("click", function (e) {
     card.classList.toggle('is-flipped');
+}); */
+
+const cards = document.querySelectorAll(".card__inner");
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        if(!card.classList.contains("is-flipped")) {
+            card.classList.add("is-flipped");
+        } else {
+            card.classList.remove("is-flipped");
+        }
+    });
 });
